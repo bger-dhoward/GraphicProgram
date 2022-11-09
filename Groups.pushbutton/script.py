3872d00c61a43137f8d8f52fc3c82ac0cb56c7c9
Dan Howard <dhoward@Ballinger.com>
2022-11-09 10:41:13 -0500

    update from 2022-08-04 changes.  added dynamo script for view filters for coloring

--- Groups.pushbutton/script.py
+++ Groups.pushbutton/script.py
@@ -1,11 +1,12 @@
-__title__ = "Groups"
-__doc__ = "Draw some groups"
+__title__ = "Graphic Program"
+__doc__ = "Generate a graphic program from CSV data file."
 __author__ = "Daniel Howard, Ballinger"
 
 #from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Transaction
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.Creation import *
 from pyrevit import revit, DB
+from pyrevit import forms
 from System.Collections.Generic import List #import ILists type
 
 import math
@@ -13,18 +14,32 @@ import csv
 import os
 
 
-os.chdir('C:\\Users\\howar\\Documents\\MypyRevitExtensions\\Test Files')
+#os.chdir('C:\\Users\\howar\\Documents\\MypyRevitExtensions\\Test Files')
 
-programCsv = open('Test Program.csv')
+forms.alert("Select CSV file with program data.", 
+    sub_msg="Click OK to browse to file location", 
+    title="Select CSV File",
+    ok=True, 
+    cancel=True, 
+    exitscript=True)
+csv_file = forms.pick_file(files_filter='CSV files | *.csv')
+
+programCsv = open(csv_file)
 programReader = csv.reader(programCsv)
-programData = list(programReader)
+rawProgramData = list(programReader)
 programCsv.close()
 
 #remove first line, since that's headers in the Excel / CSV file
-programData.pop(0)
+#programData.pop(0)
 
+programData = []
 rooms = []
 depts = {}
+
+for row in rawProgramData:
+    if row[2].upper() =="X":
+        if row[5] != "0" and row[5] != "" and row[5] != "-":
+            programData.append(row)
 
 ### Name, Quantity, Square Ft, Department, MaxWidth, MaxHeight
 
@@ -35,10 +50,27 @@ levID = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Levels).Whe
 lev = doc.GetElement(levID)
 
 filledRegionTypeCollector = list(FilteredElementCollector(doc).OfClass(DB.FilledRegionType))
-filledRegionTypeID = filledRegionTypeCollector[0].Id
+filledRegionTypeDefaultID = filledRegionTypeCollector[0].Id
 
-textTypeCollector = list(FilteredElementCollector(doc).OfClass(DB.TextNoteType))
-textTypeID = textTypeCollector[0].Id
+#create dictionary to store type IDs as values for type name key - allows selection based on dept / group name
+filledRegionTypes = {}
+frtc = FilteredElementCollector(doc).OfClass(DB.FilledRegionType)
+for frt in frtc:
+    frtname = frt.LookupParameter('Type Name').AsString()
+    frtid = frt.Id
+    filledRegionTypes[frtname] = frtid
+    
+#forms.alert(str(filledRegionTypes))
+
+textTypes = {}
+#textTypeCollector = list(FilteredElementCollector(doc).OfClass(DB.TextNoteType))
+#textTypeID = textTypeCollector[0].Id
+ttc = FilteredElementCollector(doc).OfClass(DB.TextNoteType)
+for tt in ttc:
+    ttname = tt.LookupParameter('Type Name').AsString()
+    ttid = tt.Id
+    textTypes[ttname] = ttid
+
 
 class box:
     global doc, cdoc, levID, lev
@@ -62,9 +94,17 @@ class box:
         self.L4 = Line.CreateBound(self.p4, self.p1)
 
         #if room, draw filled region, else draw detail lines
+        #not room would be a group
         if self.isRoom:
             self.cLoop = CurveLoop.Create(List[Curve]([self.L1, self.L2, self.L3, self.L4]))
-            FilledRegion.Create(doc, filledRegionTypeID, revit.active_view.Id, List[CurveLoop]([self.cLoop]))
+            deptname = self.dept
+            if deptname in filledRegionTypes:
+                filledRegionTypeId = filledRegionTypes[deptname]
+            else:
+                filledRegionTypeId = filledRegionTypeDefaultID
+            fr = FilledRegion.Create(doc, filledRegionTypeId, revit.active_view.Id, List[CurveLoop]([self.cLoop]))
+            commentsparam = fr.GetParameters("Comments")[0]
+            commentsparam.Set(deptname.upper())
         else:
             self.cArray = CurveArray()
             self.cArray.Append(self.L1)
@@ -126,12 +166,14 @@ class dRoom(box):
         
     def label(self):
         #TextNote.Create(document, viewId, XYZ, text, typeId)
-        self.textPt = XYZ(self.x, self.y + 10, 0)
-        titleText = "{name}  \n[ {qty} @ {size} NSF ]".format(name = self.name, qty = self.qty, size = self.size)
-        TextNote.Create(doc, revit.active_view.Id, self.textPt, titleText, textTypeID)
+        self.roomTextPt = XYZ(self.x, self.y + 14, 0)
+        self.infoTextPt = XYZ(self.x, self.y + 6, 0)
+        infoText = "[ {qty} @ {size} NSF ]".format(qty = self.qty, size = self.size)
+        TextNote.Create(doc, revit.active_view.Id, self.roomTextPt, self.name, textTypes["GP Room Title"])
+        TextNote.Create(doc, revit.active_view.Id, self.infoTextPt, infoText, textTypes["GP Room Info"])
 
 class group(box):
-    def __init__(self, name, size = 0, maxWidth = 200, maxHeight = 300):
+    def __init__(self, name, size = 0, maxWidth = 200, maxHeight = 300, grouporder=0):
         self.rooms = []
         self.isRoom = False
         self.name = name
@@ -151,20 +193,37 @@ class group(box):
         self.offset = 5
         self.currentx = 0
         self.currenty = 0
+        self.netarea = 0
+        self.grouporder = grouporder
 
     def addroom(self, room):
         self.rooms.append(room)
 
-    def setstarts(self):
-        self.xstart = self.x + self.margin
-        self.ystart = self.y - self.voffset
-        self.currentx = self.xstart
-        self.currenty = self.ystart
+#    def setstarts(self):
+#        self.xstart = self.x + self.margin
+#        self.ystart = self.y - self.voffset
+#        self.currentx = self.xstart
+#        self.currenty = self.ystart
 
-    def drawrooms(self):
+#    def drawrooms(self):
+#        for room in self.rooms:
+#            room.draw(currentx, currenty)
+#            self.currentx = self.currentx + self.offset + room.width
+            
+    def label(self):
+        #TextNote.Create(document, viewId, XYZ, text, typeId)
+        self.textPt = XYZ(self.x, self.y + 20, 0)
+        titleText = "{name} - {area} NSF".format(name = self.name, area = self.netarea)
+        TextNote.Create(doc, revit.active_view.Id, self.textPt, titleText, textTypes["GP Dept Title"])
+        
+    def totalArea(self):
         for room in self.rooms:
-            room.draw(currentx, currenty)
-            self.currentx = self.currentx + self.offset + room.width
+            if room.qty > 1:
+                netroomarea = room.size * room.qty
+            else:
+                netroomarea = room.size
+            self.netarea += netroomarea
+                
             
 
 
@@ -175,19 +234,29 @@ t.Start()
 xoffset = 2
 yoffset = 2
 ybreak = 20
+yMroomBreak = 2 # for multiple room breaks
 origx = 0
 origy = 0
 
+#generate room data structure from data pulled from csv file
 for room in programData:
-    name = room[0]
-    qty = room[1]
-    size = room[2]
-    dept = room[3]
-    maxWidth = room[4]
-    maxHeight = room[5]
-    
+#    name = room[0].upper()
+#    qty = room[1]
+#    size = room[2]
+#    dept = room[3].upper()
+#    maxWidth = room[4]
+#    maxHeight = room[5]
+    name = room[4].upper()
+    qty = room[5]
+    size = room[6]
+    dept = room[0]
+    maxWidth = None
+    maxHeight = None
+    deptOrder = room[1]
+
+
     if dept not in depts:
-        depts[dept] = group(dept)
+        depts[dept] = group(dept, grouporder=deptOrder)
     
     dR = dRoom(name, qty, size, dept, maxWidth, maxHeight)
     depts[dept].addroom(dR)
@@ -207,22 +276,37 @@ for room in programData:
         dR.label()
         origy -= (dR.height + ybreak)
 """
+#for deptname, dept in sorted(depts.items(), key=lambda item: item[1].grouporder):
 for dept in depts.values():
+    dept.totalArea()
     dept.draw(origx, origy)
+    dept.label()
+    rOrigx = origx + xoffset
+    rOrigy = origy - ybreak
+    maxRheight = 0
     for room in dept.rooms:
         room.rect()
+#        if rOrigx + room.width + 5 >= origx + dept.width:
+#            rOrigx = origx + xoffset
+#            rOrigy -= maxRheight + ybreak
         if room.qty > 1:
             for i in range(room.qty):
-                room.draw(origx, origy)
                 if i == 0:
+                    room.draw(rOrigx, rOrigy)
                     room.label()
-                origx += (room.height + xoffset)
-            origx = 0
-            origy -= (room.height + ybreak)
+                elif rOrigx + (2 *room.width) + xoffset >= dept.xw:
+                    rOrigx = dept.x + xoffset
+                    rOrigy -= (room.height + yMroomBreak)
+                    room.draw(rOrigx, rOrigy)
+                else:
+                    rOrigx += (room.width + xoffset)
+                    room.draw(rOrigx, rOrigy)
         else:
-            room.draw(origx, origy)
+            room.draw(rOrigx, rOrigy)
             room.label()
-            origy -= (room.height + ybreak)
+        rOrigx = dept.x + xoffset
+        rOrigy -= (room.height + ybreak)
+
     origx += dept.width + 5
     origy = 0
 
@@ -232,3 +316,4 @@ for dept in depts.values():
 
 
 t.Commit()
+#print(depts)
